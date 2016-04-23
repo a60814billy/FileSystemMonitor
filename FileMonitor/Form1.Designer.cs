07b5c248dbeeac13820b966462ae91912e6d6cc9
Raccoon <a60814billy@gmail.com>
2016-04-24 01:07:57 +0800

    initialize commit * version 1.5

---
+++ FileMonitor/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace FileMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.directoryPathTextBox = new System.Windows.Forms.TextBox();
+            this.btn_browser = new System.Windows.Forms.Button();
+            this.directoryWatcher = new System.IO.FileSystemWatcher();
+            this.directoryBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.filesDataGridView = new System.Windows.Forms.DataGridView();
+            this.dgcol_subdir = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgcol_count = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgcol_filesize = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgcol_lastupdate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.directoryWatcher)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filesDataGridView)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Directory";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // directoryPathTextBox
+            // 
+            this.directoryPathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.directoryPathTextBox.Location = new System.Drawing.Point(93, 3);
+            this.directoryPathTextBox.Name = "directoryPathTextBox";
+            this.directoryPathTextBox.Size = new System.Drawing.Size(405, 22);
+            this.directoryPathTextBox.TabIndex = 1;
+            // 
+            // btn_browser
+            // 
+            this.btn_browser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_browser.Location = new System.Drawing.Point(504, 3);
+            this.btn_browser.Name = "btn_browser";
+            this.btn_browser.Size = new System.Drawing.Size(74, 25);
+            this.btn_browser.TabIndex = 2;
+            this.btn_browser.Text = "Browser";
+            this.btn_browser.UseVisualStyleBackColor = true;
+            this.btn_browser.Click += new System.EventHandler(this.btn_browser_Click);
+            // 
+            // directoryWatcher
+            // 
+            this.directoryWatcher.EnableRaisingEvents = true;
+            this.directoryWatcher.SynchronizingObject = this;
+            // 
+            // filesDataGridView
+            // 
+            this.filesDataGridView.AllowUserToAddRows = false;
+            this.filesDataGridView.AllowUserToDeleteRows = false;
+            this.filesDataGridView.AllowUserToOrderColumns = true;
+            this.filesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.filesDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgcol_subdir,
+            this.dgcol_count,
+            this.dgcol_filesize,
+            this.dgcol_lastupdate});
+            this.filesDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.filesDataGridView.Location = new System.Drawing.Point(5, 0);
+            this.filesDataGridView.Name = "filesDataGridView";
+            this.filesDataGridView.ReadOnly = true;
+            this.filesDataGridView.RowTemplate.Height = 24;
+            this.filesDataGridView.Size = new System.Drawing.Size(581, 270);
+            this.filesDataGridView.TabIndex = 3;
+            this.filesDataGridView.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.filesDataGridView_ColumnHeaderMouseClick);
+            this.filesDataGridView.RowPrePaint += new System.Windows.Forms.DataGridViewRowPrePaintEventHandler(this.filesDataGridView_RowPrePaint);
+            // 
+            // dgcol_subdir
+            // 
+            this.dgcol_subdir.HeaderText = "Directory Name";
+            this.dgcol_subdir.Name = "dgcol_subdir";
+            this.dgcol_subdir.ReadOnly = true;
+            // 
+            // dgcol_count
+            // 
+            this.dgcol_count.HeaderText = "File Count";
+            this.dgcol_count.Name = "dgcol_count";
+            this.dgcol_count.ReadOnly = true;
+            // 
+            // dgcol_filesize
+            // 
+            this.dgcol_filesize.HeaderText = "Total File Size";
+            this.dgcol_filesize.Name = "dgcol_filesize";
+            this.dgcol_filesize.ReadOnly = true;
+            // 
+            // dgcol_lastupdate
+            // 
+            this.dgcol_lastupdate.HeaderText = "Last Update";
+            this.dgcol_lastupdate.Name = "dgcol_lastupdate";
+            this.dgcol_lastupdate.ReadOnly = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel1);
+            this.splitContainer1.Panel1.Padding = new System.Windows.Forms.Padding(5, 5, 5, 0);
+            this.splitContainer1.Panel1MinSize = 30;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.filesDataGridView);
+            this.splitContainer1.Panel2.Padding = new System.Windows.Forms.Padding(5, 0, 5, 5);
+            this.splitContainer1.Size = new System.Drawing.Size(591, 315);
+            this.splitContainer1.SplitterDistance = 36;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.94872F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 82.05128F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 79F));
+            this.tableLayoutPanel1.Controls.Add(this.btn_browser, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.directoryPathTextBox, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(5, 5);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(581, 31);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 315);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "FileMonitor";
+            ((System.ComponentModel.ISupportInitialize)(this.directoryWatcher)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filesDataGridView)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox directoryPathTextBox;
+        private System.Windows.Forms.Button btn_browser;
+        private System.IO.FileSystemWatcher directoryWatcher;
+        private System.Windows.Forms.FolderBrowserDialog directoryBrowserDialog;
+        private System.Windows.Forms.DataGridView filesDataGridView;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgcol_subdir;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgcol_count;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgcol_filesize;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgcol_lastupdate;
+    }
+}
+
---
+++ FileMonitor/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.Sql;
+using System.Data;
+
+namespace FileMonitor
+{
+    public partial class Form1 : Form
+    {
+        private String directoryName = "";
+        private Timer waitTimeer = new Timer();
+        private bool dirty = false;
+        public Form1()
+        {
+            InitializeComponent();
+            initDirectoryWatcher();
+            filesDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            waitTimeer.Interval = 3000;
+            waitTimeer.Tick += WaitTimeer_Tick;
+        }
+
+        private void WaitTimeer_Tick(object sender, EventArgs e)
+        {
+            waitTimeer.Enabled = false;
+            if (this.dirty)
+            {
+                this.listSubDirFileCounts();
+            }
+        }
+
+        private void initDirectoryWatcher()
+        {
+            directoryWatcher.Created += onDirectoryChangeed;
+            directoryWatcher.Changed += onDirectoryChangeed;
+            directoryWatcher.Deleted += onDirectoryChangeed;
+            directoryWatcher.Renamed += onDirectoryChangeed;
+        }
+
+        public void onDirectoryChangeed(object source, FileSystemEventArgs e)
+        {
+            if (this.dirty && this.waitTimeer.Enabled) return;
+            if (this.waitTimeer.Enabled && !this.dirty)
+            {
+                this.dirty = true;
+            }
+            else
+            {
+                this.waitTimeer.Enabled = true;
+                this.listSubDirFileCounts();
+            }
+        }
+
+        private void btn_browser_Click(object sender, EventArgs e)
+        {
+            DialogResult result = directoryBrowserDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                directoryName = directoryBrowserDialog.SelectedPath;
+                directoryPathTextBox.Text = directoryName;
+                this.listSubDirFileCounts();
+                setFileSystemWatcher();
+            }
+        }
+
+        private void setFileSystemWatcher()
+        {
+            if (directoryName != "")
+            {
+                directoryWatcher.Path = directoryName;
+                directoryWatcher.NotifyFilter = System.IO.NotifyFilters.Attributes | System.IO.NotifyFilters.CreationTime | System.IO.NotifyFilters.DirectoryName | System.IO.NotifyFilters.FileName | System.IO.NotifyFilters.LastAccess | System.IO.NotifyFilters.LastWrite | System.IO.NotifyFilters.Security | System.IO.NotifyFilters.Size;
+                directoryWatcher.Filter = "*.*";
+                directoryWatcher.IncludeSubdirectories = true;
+                directoryWatcher.EnableRaisingEvents = true;
+            }
+        }
+
+        private void listSubDirFileCounts()
+        {
+            filesDataGridView.Rows.Clear();
+            List<String> subdirList = new List<string>(Directory.GetDirectories(directoryName));
+            foreach (string subdirname in subdirList)
+            {
+                string pathName = Path.GetFileName(subdirname);
+                string[] files = Directory.GetFiles(subdirname, "*.*", SearchOption.AllDirectories);
+                int count = files.Length;
+                long fileSize = 0;
+                DateTime lastUpdate = new DateTime(1991, 1, 1);
+
+                foreach (string filename in files)
+                {
+                    FileInfo info = new FileInfo(filename);
+                    fileSize += info.Length;
+                    if (lastUpdate.CompareTo(info.LastWriteTime) <= 0)
+                    {
+                        lastUpdate = info.LastWriteTime;
+                    }
+                }
+                filesDataGridView.Rows.Add(new Object[] { pathName, count, humanReadable(fileSize), lastUpdate });
+                if (filesDataGridView.SortedColumn != null)
+                {
+                    if (filesDataGridView.SortOrder == SortOrder.Ascending)
+                    {
+                        filesDataGridView.Sort(filesDataGridView.SortedColumn, System.ComponentModel.ListSortDirection.Ascending);
+                    }
+                    else
+                    {
+                        filesDataGridView.Sort(filesDataGridView.SortedColumn, System.ComponentModel.ListSortDirection.Descending);
+                    }
+                }
+            }
+        }
+
+        private void filesDataGridView_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
+        {
+            try
+            {
+                if ((int)(filesDataGridView.Rows[e.RowIndex].Cells[1].Value) == 0)
+                {
+                    DataGridViewRow row = filesDataGridView.Rows[e.RowIndex];
+                    row.DefaultCellStyle.BackColor = System.Drawing.Color.Red;
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private string humanReadable(long len)
+        {
+            string[] sizes = { "B", "KB", "MB", "GB" };
+            int order = 0;
+            while (len >= 1024 && order + 1 < sizes.Length)
+            {
+                order++;
+                len = len / 1024;
+            }
+
+            // Adjust the format string to your preferences. For example "{0:0.#}{1}" would
+            // show a single decimal place, and no space.
+            string result = String.Format("{0:0.##} {1}", len, sizes[order]);
+            return result;
+        }
+
+        private void filesDataGridView_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+
+        }
+    }
+}
